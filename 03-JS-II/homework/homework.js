49de646298e1be411d00597c48404c14b3a6162e
pablo_andrada <pablomatiasandrada@gmail.com>
2022-05-13 19:42:03 -0300

    commit_15

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -97,7 +97,9 @@ function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
-  if(numero > 20 && numero < 50) {
+  if(numero < 50) {
+    return "true";
+  } else if (numero > 20) { 
     return "true";
   } else {
     return "false";
