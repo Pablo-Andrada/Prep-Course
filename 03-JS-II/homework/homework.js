14a751bbdab513cd0ce6cfb099996861bc2c5098
pablo_andrada <pablomatiasandrada@gmail.com>
2022-05-16 17:47:42 -0300

    commit_17

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -129,19 +129,19 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
-  if ((num1 > num2 && num1 > num3 && num1 > 0)){
-    return "Numero 1 es mayor y positivo";
-  } 
-    else if (num1 < 0 || num2 < 0 || num3 < 0) {
+  if(num1 < 0 || num2 < 0 || num3 < 0) {
     return "Hay negativos";
-  } 
-    else if ( num3 > num1 && num3 > num2){
-    return num3 + 1;
-  } 
-    else if (num1 === 0 || num2 === 0 || num3 === 0) {
+  }
+  else if(num1 === 0 || num2 === 0 || num3 === 0) {
     return "Error";
-  } 
-    else {
+  }
+  else if(num1 > 0 && num1 > num2 && num1 > num3) {
+    return "Número 1 es mayor y positivo";
+  }
+  else if(num3 > num1 && num3 > num2) {
+    return num3 + 1;
+  }
+  else {
     return false;
   }
 }
@@ -152,12 +152,29 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+  if (numero < 2) {
+    return false;
+  } 
+  else if (numero === 2) {
+    return === true;
+  }
+  for (var i = 2; i < numero ; i++){
+    if (numero % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
+  if(valor === Verdadero){
+    return "Soy verdadero";
+  } else {
+    return "Soy falso";
+  }
 
 }
 
@@ -166,6 +183,10 @@ function tablaDelSeis(){
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
   
+  for (numero = 0; numero > 11; numero ++){
+    var tabla6 = numero * 6;
+  }
+  return tabla6;
 }
 
 function tieneTresDigitos(numero){
